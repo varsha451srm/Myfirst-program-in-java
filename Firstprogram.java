3ae83556e44f30d40e6a3fcfafea928fd60f1d4b
varsha451srm <99247917+varsha451srm@users.noreply.github.com>
2023-02-22 17:12:35 +0530

    Add files via upload

---
+++ Firstprogram.java
@@ -0,0 +1,19 @@
+class CMDL
+{
+	public static void main(String [] args)
+	{
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+}
+class CMDL1
+{
+	public static void main(String [] args)
+	{
+		int a,b;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		System.out.println("Sum :" +(a+b));
+	}
+}
